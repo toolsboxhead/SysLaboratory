c1955a441f965ea55256beb77d0eed45ad46b312
ToolsBoxHead <jesus_im.salvador@protonmail.com>
2023-08-01 10:56:09 -0400

    Respaldar cambios automáticamente

--- gitauto.py
+++ gitauto.py
@@ -3,6 +3,6 @@ import subprocess
 def git_commit_push():
     subprocess.run(["git", "add", "."])
     subprocess.run(["git", "commit", "-m", "Respaldar cambios automáticamente"])
-    subprocess.run(["git", "push", "origin", "main"])
+    
 
 git_commit_push()
